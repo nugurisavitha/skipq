bc0d886a634d4463fd8f7f2ac280ca40024fb8ec
eshwar2468 <122964646+eshwar2468@users.noreply.github.com>
2026-04-19 08:29:16 +0530

    feat(mobile): add mobile/src/context/AuthContext.js

---
+++ mobile/src/context/AuthContext.js
@@ -0,0 +1,4 @@
+import React, { createContext, useState, useEffect, useCallback } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../services/api';
+
